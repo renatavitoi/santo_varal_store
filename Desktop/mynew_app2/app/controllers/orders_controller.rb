bf8167da211f4f09a5e50621564267e19312491a
renatavitoi <renata_band@hotmail.com>
2019-08-20 19:55:42 +0200

    5.4 fixed authentications

---
+++ Desktop/mynew_app2/app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+  def index
+    @orders = Order.includes(:product).all
+  end
+
+  def show
+    @order= Order.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
