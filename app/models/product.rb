09d58f9720ae005fc830f2cddd8d0fd7da5299fe
renatavitoi <renata_band@hotmail.com>
2019-09-15 11:34:12 +0200

    5.7 comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,16 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
+
 	def self.search(search_term)
 		if Rails.env.production? # use "ilike" matching operator for postgres to ignore upper- and lower-case
 			Product.where("name ilike ?", "%#{search_term}%")
 		else
 			Product.where("name LIKE ?", "%#{search_term}%")
 		end
-  end
+   end
+	end
+
+def highest_rating_comment
+  comments.rating_desc.first
 end
