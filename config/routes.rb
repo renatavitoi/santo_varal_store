6ddb9e6893ab43331b98bf8d778f58cf54c0a65c
renatavitoi <renata_band@hotmail.com>
2019-09-20 13:45:29 +0200

    5.8 fixed comments controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   resources :products do
     resources :comments
   end
-
   resources :orders, only: [:index, :show, :create, :destroy]
 
   get 'simple_pages/about'
