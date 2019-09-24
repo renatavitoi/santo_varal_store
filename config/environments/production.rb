761901b7d2d92e208b22978548eefe41b3637819
renatavitoi <renata_band@hotmail.com>
2019-09-24 05:42:49 +0200

    5.8 fixed comment form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,5 +92,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'santovaralstore.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'secure-sea-81066.herokuapp.com' }
 end
