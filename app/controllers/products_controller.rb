28536cac480f583cb87cbf0c7baab7a405c7f7cd
renatavitoi <renata_band@hotmail.com>
2019-09-29 15:33:37 +0200

    5.9 fixed validation and pagination

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -55,10 +55,11 @@ class ProductsController < ApplicationController
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
-        redirect_to "/simple_pages/landing_page"
     end
   end
 end
+
+
   # DELETE /products/1
   # DELETE /products/1.json
   def destroy
