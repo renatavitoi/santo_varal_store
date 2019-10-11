104681b077eddd669c1fadd6976ba92bee3b2680
renatavitoi <43076004+renatavitoi@users.noreply.github.com>
2019-10-11 18:27:21 +0200

    Revert "Comment"

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,7 +6,6 @@ class Ability
     if user.admin?
       can :manage, :all
     else
-      can :read, :all
       can [:show, :edit, :update], User, id: user.id
       can [:show, :index], Order, user_id: user.id
       can :create, Comment, user_id: user.id
