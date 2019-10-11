87de7b1f43b6725084ce342a68e84a53b0b0ad48
renatavitoi <renata_band@hotmail.com>
2019-10-11 17:42:47 +0200

    5.10 added delete comment as admin

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -3,6 +3,14 @@ class Ability
 
   def initialize(user)
     user ||= User.new # guest user (not logged in)
-    can :manage, User, id: user.id
-  end
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can [:show, :edit, :update], User, id: user.id
+      can [:show, :index], Order, user_id: user.id
+      can :create, Comment, user_id: user.id
+      can [:show, :index], Product
+     end
+   end
 end
