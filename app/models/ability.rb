56f36967cd7eb3f0b9e36d549da7d9f4d7583dc8
renatavitoi <renata_band@hotmail.com>
2019-10-13 18:36:55 +0200

    fix file conflicts

---
+++ app/models/ability.rb
@@ -0,0 +1,16 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, :all
+    else
+      can :read, :all
+      can [:show, :edit, :update], User, id: user.id
+      can [:show, :index], Order, user_id: user.id
+      can :create, Comment, user_id: user.id
+      can [:show, :index], Product
+     end
+   end
+end
