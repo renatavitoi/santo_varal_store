6915dc13dd7f72a8eae6cd3d17681779ddf4fdf8
renatavitoi <renata_band@hotmail.com>
2019-10-25 11:37:32 +0200

    new images

--- Desktop/santo_varal_store/db/schema.rb
+++ Desktop/santo_varal_store/db/schema.rb
@@ -55,3 +55,7 @@ ActiveRecord::Schema.define(version: 2019_10_08_043645) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
+end
