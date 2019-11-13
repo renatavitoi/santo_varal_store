c0cad49681e8e8e2cee7d2dbe2d7e8d56b11c91c
renatavitoi <renata_band@hotmail.com>
2019-11-13 17:27:01 +0100

    Remove extra blank lines

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -5,7 +5,6 @@ describe UsersController, type: :controller do
     let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
 
     context 'when a user is logged in' do
-
       before do
         sign_in user
       end
@@ -15,7 +14,6 @@ describe UsersController, type: :controller do
         expect(response).to be_ok
         expect(assigns(:user)).to eq user
       end
-
     end
 
     context 'when a user is not logged in' do  
