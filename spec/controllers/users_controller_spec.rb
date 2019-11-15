454431356db71a186ac04042142e6d07cdfc9dc5
renatavitoi <renata_band@hotmail.com>
2019-11-15 12:09:48 -0400

    Add functional test to post at create signed as admin

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,6 +1,26 @@
 require 'rails_helper'
 
 describe UsersController, type: :controller do
+
+  describe 'POST #create signed as admin' do 
+    let(:admin) { User.create!(email: 'admin@example.com', password: '1234567890', admin: true) }
+
+    before do
+      sign_in admin
+    end
+    
+    context 'when a user have first_name last_name email and password' do 
+      let(:user) { User.new(first_name: 'Peter', last_name: 'Example', email: 'peter@example.com', password: '1234567890') }
+
+      it "creates an user and redirect to user view" do
+        post :create, params: { user: { first_name: user.first_name, last_name: user.last_name, email: user.email, password: user.password } }
+        expect(assigns(:user)).to_not be_nil
+        expect(response).to redirect_to user_path(assigns(:user))
+      end
+      
+    end
+  end
+
   describe 'GET #show' do
     let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
 
