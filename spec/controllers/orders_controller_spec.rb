3083f31529b876a7c2504e4fbdb02ef84eb6ada0
renatavitoi <renata_band@hotmail.com>
2019-11-15 21:03:13 +0100

    Add functional test to order controller when user is signed in as an admin, signout and signing but not admin

--- spec/controllers/orders_controller_spec.rb
+++ spec/controllers/orders_controller_spec.rb
@@ -1,191 +1,73 @@
 require 'rails_helper'
 
 describe OrdersController, type: :controller do
-  describe 'User default signed in' do
-    let(:userdefault) { User.create!(email: 'admin@example.com', password: '1234567890') }
-    
-    before do
-      sign_in userdefault
-    end
-    
-    describe 'GET #index' do 
-      # it 'redirect to root_url' do
-      #   get :index
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #new' do 
-      # it 'redirect to root_url' do
-      #   get :new
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'POST #create' do 
-      # it 'redirect to root_url' do
-      #   post :create, params: { user: { first_name: '', last_name: '', email: '', password: '' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #edit' do 
-      # it 'redirect to edit_user' do
-      #   get :edit, params: { id: userdefault.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(edit_user_registration_path)
-      # end
-    end
-    
-    describe 'PUT #update' do 
-      # it 'return user' do
-      #   put :update, params: { id: userdefault.id, user: { first_name: 'Jhon' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(user_path(userdefault))
-      # end
-    end
-    
-    describe 'GET #show' do 
-      # it 'returns user' do
-      #   get :show, params: { id: userdefault.id }
-      #   expect(assigns(:user)).to_not be_nil
-      #   expect(assigns(:user)).to eq userdefault
-      # end
-    end
-    
-    describe 'DELETE #destroy' do 
-      # it 'redirect to root_url' do
-      #   delete :destroy, params: { id: userdefault.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-  end
-  
-  describe 'User signed out' do
-    let(:userdefault) { User.create!(email: 'admin@example.com', password: '1234567890') }
-    
-    describe 'GET #index' do 
-      # it 'redirect to root url' do
-      #   get :index
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #new' do 
-      # it 'redirect to signin' do
-      #   get :new
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(new_user_session_path)
-      # end
-    end
-    
-    describe 'POST #create' do 
-      # it 'redirect to signin' do
-      #   post :create, params: { user: { first_name: '', last_name: '', email: '', password: '' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(new_user_session_path)
-      # end
-    end
-    
-    describe 'GET #edit' do 
-      # it 'redirect to signin' do
-      #   get :edit, params: { id: userdefault.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(new_user_session_path)
-      # end
-    end
-    
-    describe 'PUT #update' do 
-      #   it 'redirect to signin' do
-      #     put :update, params: { id: userdefault.id, user: { first_name: 'Jhon' } }
-      #     expect(response).to have_http_status(302)
-      #   end
-    end
-    
-    describe 'GET #show' do 
-      # it 'redirect to root_url' do
-      #   get :show, params: { id: userdefault.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'DELETE #destroy' do 
-      # it 'redirect to signin' do
-      #   delete :destroy, params: { id: userdefault.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(new_user_session_path)
-      # end
-    end
-  end
-  
-  describe 'User admin signed in' do
+  let!(:product) { Product.create!(name: "race bike") }
+  let(:user) { User.create!(email: "santovaralstore@gmail.com", password: "password") }
+  let!(:order) { Order.create!(user: user, product: product) }
+
+  describe 'User is admin' do
     let(:admin) { User.create!(email: 'admin@example.com', password: '1234567890', admin: true) }
+    let!(:orderadmin) { Order.create!(user: admin, product: product) }
     
     before do
       sign_in admin
     end
     
     describe 'GET #index' do 
-      # it 'returns all users' do
-      #   get :index
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:users).empty?).to be false 
-      # end
-    end
-    
-    describe 'GET #new' do 
-      # it 'returns new user' do
-      #   get :new
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:user)).to_not be_nil
-      # end
-    end
-    
-    describe 'POST #create' do 
-      # it 'creates new user' do
-      #   post :create, params: { user: { first_name: 'jhon', last_name: 'due', email: 'jhon@example.com', password: 'password' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(user_path(assigns(:user)))
-      # end
-    end
-    
-    describe 'GET #edit' do 
-      # it 'redirects to registration_path' do
-      #   get :edit, params: { id: admin.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(edit_user_registration_path)
-      # end
-    end
-    
-    describe 'PUT #update' do 
-      # it 'updates user' do
-      #   put :update, params: { id: admin.id, user: { first_name: 'james' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(user_path(admin))
-      #   expect(assigns(:user)).to eq admin
-      # end
+      it 'returns all orders' do
+        get :index
+        expect(response).to have_http_status(200)
+        expect(assigns(:orders).empty?).to be false 
+      end
     end
     
     describe 'GET #show' do 
-      # it 'return user' do
-      #   get :show, params: { id: admin.id }
-      #   expect(assigns(:user)).to_not be_nil
-      #   expect(assigns(:user)).to eq admin
-      # end
+      it 'return order' do
+        get :show, params: { id: admin.id }
+        expect(assigns(:order)).to_not be_nil
+        expect(assigns(:order)).to eq orderadmin
+      end
+    end
+  end
+
+  describe 'User is signed in' do
+
+    before do
+      sign_in user
     end
     
-    describe 'DELETE #destroy' do 
-      # it 'destroys user' do
-      #   delete :destroy, params: { id: admin.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(users_path)
-      # end
+    describe 'GET #index' do 
+      it 'returns all orders' do
+        get :index
+        expect(response).to have_http_status(200)
+        expect(assigns(:orders).empty?).to be false 
+      end
+    end
+    
+    describe 'GET #show' do 
+      it 'returns order' do
+        get :show, params: { id: order.id }
+        expect(assigns(:order)).to_not be_nil
+        expect(assigns(:order)).to eq order
+      end
+    end
+  end
+  
+  describe 'User is not signed in' do  
+    describe 'GET #index' do 
+      it 'redirect to signin' do
+        get :index
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+    
+    describe 'GET #show' do 
+      it 'redirect to signin' do
+        get :show, params: { id: order.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
     end
   end
 end
