63c536fba0b96a3ff89a28478fa1bb7dc5bd33ca
renatavitoi <renata_band@hotmail.com>
2019-11-15 21:46:27 +0100

    Add functional test to comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,11 @@
 class CommentsController < ApplicationController
   load_and_authorize_resource
-
+  
   def create
     @product = Product.find(params[:product_id])
     @comment = @product.comments.new(comment_params)
     @comment.user = current_user
-
+    
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @product, notice: 'Review was created successfully.' }
@@ -15,9 +15,9 @@ class CommentsController < ApplicationController
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
-end
-
-
+  end
+  
+  
   def destroy
     @comment = Comment.find(params[:id])
     product = @comment.product
@@ -25,16 +25,7 @@ end
     redirect_to product
   end
 
-
-
-
-
-
-
-
-
-private
-
+  private  
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -12,53 +12,20 @@ describe CommentsController, type: :controller do
       sign_in admin
     end
 
-    describe 'GET #index' do 
-      # it 'returns all products' do
-      #   get :index
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:products).empty?).to be false 
-      # end
-    end
-
-    describe 'GET #new' do 
-      # it 'returns new product' do
-      #   get :new
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:product)).to_not be_nil
-      # end
-    end
-
     describe 'POST #create' do 
-      # it 'creates new product' do
-      #   post :create, params: { product: { name: 'product' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(product_path(assigns(:product)))
-      # end
-    end
-
-    describe 'PUT #update' do 
-      # it 'updates product' do
-      #   put :update, params: { id: product.id, product: { name: 'some product' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(product_path(product))
-      #   expect(assigns(:product)).to eq product
-      # end
-    end
-
-    describe 'GET #show' do 
-      # it 'return product' do
-      #   get :show, params: { id: product.id }
-      #   expect(assigns(:product)).to_not be_nil
-      #   expect(assigns(:product)).to eq product
-      # end
+      it 'create comment' do
+        post :create, params: { product_id: product.id, comment: { body: 'comment', user: user } }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(product_path(assigns(:product)))
+      end
     end
 
     describe 'DELETE #destroy' do 
-      # it 'destroys product' do
-      #   delete :destroy, params: { id: product.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(products_path)
-      # end
+      it 'redirect to root_url' do
+        delete :destroy, params: { product_id: product.id, id: comment.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(product_path(product))
+      end
     end
   end
   
@@ -67,118 +34,38 @@ describe CommentsController, type: :controller do
       sign_in user
     end
 
-    describe 'GET #index' do 
-      # it 'returns all products' do
-      #   get :index
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:products).empty?).to be false 
-      # end
-    end
-    
-    describe 'GET #new' do 
-      # it 'redirect to root_url' do
-      #   get :new
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
     describe 'POST #create' do 
-      # it 'redirect to root_url' do
-      #   post :create, params: { product: { first_name: '', last_name: '', email: '', password: '' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #edit' do 
-      # it 'redirect to root_url' do
-      #   get :edit, params: { id: product.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'PUT #update' do 
-      # it 'redirect to root_url' do
-      #   put :update, params: { id: product.id, user: { first_name: 'Jhon' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #show' do 
-      # it 'redirect to root_url' do
-      #   get :show, params: { id: product.id }
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:product)).to eq product
-      # end
+      it 'create comment' do
+        post :create, params: { product_id: product.id, comment: { body: 'comment', user: user } }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(product_path(assigns(:product)))
+      end
     end
     
     describe 'DELETE #destroy' do 
-      # it 'redirect to root_url' do
-      #   delete :destroy, params: { id: product.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
+      it 'redirect to root_url' do
+        delete :destroy, params: { product_id: product.id, id: comment.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
     end
   end
   
   context 'User is not signed in' do
-    describe 'GET #index' do 
-      # it 'returns all products' do
-      #   get :index
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:products).empty?).to be false 
-      # end
-    end
-    
-    describe 'GET #new' do 
-      # it 'redirect to root_url' do
-      #   get :new
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
     describe 'POST #create' do 
-      # it 'redirect to root_url' do
-      #   post :create, params: { product: { first_name: '', last_name: '', email: '', password: '' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #edit' do 
-      # it 'redirect to root_url' do
-      #   get :edit, params: { id: product.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'PUT #update' do 
-      # it 'redirect to root_url' do
-      #   put :update, params: { id: product.id, user: { first_name: 'Jhon' } }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
-    end
-    
-    describe 'GET #show' do 
-      # it 'redirect to root_url' do
-      #   get :show, params: { id: product.id }
-      #   expect(response).to have_http_status(200)
-      #   expect(assigns(:product)).to eq product
-      # end
+      it 'create comment' do
+        post :create, params: { product_id: product.id, comment: { body: 'comment', user: user } }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(product_path(assigns(:product)))
+      end
     end
     
     describe 'DELETE #destroy' do 
-      # it 'redirect to root_url' do
-      #   delete :destroy, params: { id: product.id }
-      #   expect(response).to have_http_status(302)
-      #   expect(response).to redirect_to(root_url)
-      # end
+      it 'redirect to root_url' do
+        delete :destroy, params: { product_id: product.id, id: comment.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
     end
   end
 end
