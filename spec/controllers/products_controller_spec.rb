5339a6b1247998bcfb2ab6b768837befa4fe46dc
renatavitoi <renata_band@hotmail.com>
2019-11-15 20:20:38 +0100

    Add functional test to product controller when user is signed in but is not admin

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -11,8 +11,64 @@ describe ProductsController, type: :controller do
   end
   
   context 'User is signed in' do
-    it '' do
-      
+    before do
+      sign_in user
+    end
+
+    describe 'GET #index' do 
+      it 'returns all products' do
+        get :index
+        expect(response).to have_http_status(200)
+        expect(assigns(:products).empty?).to be false 
+      end
+    end
+    
+    describe 'GET #new' do 
+      it 'redirect to root_url' do
+        get :new
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+    
+    describe 'POST #create' do 
+      it 'redirect to root_url' do
+        post :create, params: { product: { first_name: '', last_name: '', email: '', password: '' } }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+    
+    describe 'GET #edit' do 
+      it 'redirect to root_url' do
+        get :edit, params: { id: product.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+    
+    describe 'PUT #update' do 
+      it 'redirect to root_url' do
+        put :update, params: { id: product.id, user: { first_name: 'Jhon' } }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
+    end
+    
+    describe 'GET #show' do 
+      it 'redirect to root_url' do
+        get :show, params: { id: product.id }
+        expect(response).to have_http_status(200)
+        expect(assigns(:product)).to eq product
+      end
+    end
+    
+    describe 'DELETE #destroy' do 
+      it 'redirect to root_url' do
+        delete :destroy, params: { id: product.id }
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(root_url)
+      end
     end
   end
   
