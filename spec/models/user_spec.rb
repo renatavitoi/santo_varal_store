5e37eb1e285556ef5d0cc52180a388e3ed823a60
renatavitoi <renata_band@hotmail.com>
2019-11-20 13:42:08 +0100

    add factory to build user on user model unit test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe User do
   context "when the user has not email and password" do
-    let(:user) { User.new() }
+    let(:user) { FactoryBot.build(:user, email: "not_an_email") }
 
     it "returns a user invalid" do
       expect(user).not_to be_valid
