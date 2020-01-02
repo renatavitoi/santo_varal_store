4c06d0e193abd7f95266f7d783ba961cb3bf7ddf
renatavitoi <renata_band@hotmail.com>
2020-01-01 23:23:48 -0400

    6.14 fixed product.js

--- app/assets/javascript/channels/product.js
+++ app/assets/javascript/channels/product.js
@@ -2,28 +2,26 @@ $(document).on('turbolinks:load', function() {
 });
 
 App.product = App.cable.subscriptions.create("ProductChannel", {
-connected: function() {
-App.product.listenToComments()
-// Called when the subscription is ready for use on the server
-},
+  connected: function() {
+    App.product.listenToComments()
+    // Called when the subscription is ready for use on the server
+  },
 
-disconnected: function() {
-// Called when the subscription has been terminated by the server
-},
+  disconnected: function() {
+    // Called when the subscription has been terminated by the server
+  },
 
-received: function(data) {
+  received: function(data) {
     // Called when there's incoming data on the websocket for this channel
-$(".alert.alert-info").show();
-$('#new-comment').prepend(data.comment);
-$("#average-rating").attr('data-score', data.average_rating);
-refreshRating();
-},
+    $(".alert.alert-info").fadeToggle(3000);
+    $('.new-comment').prepend(data.comment);
+    $("#average-rating").attr('data-score', data.average_rating);
+    refreshRating();
+  },
 
-listenToComments: function() {
-return this.perform('listen', {
-product_id: $("[data-product-id]").data("product-id")
-});
-}
-});
-
+  listenToComments: function() {
+    return this.perform('listen', {
+      product_id: $("[data-product-id]").data("product-id")
+    });
+  }
 });
