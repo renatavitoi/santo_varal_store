9de76712abb7d3d85a82f4b40a0905c0fb0d0a88
renatavitoi <renata_band@hotmail.com>
2020-01-01 23:34:17 -0400

    6.14 fixed comment_update_job.rb

--- app/jobs/comment_update_job.rb
+++ app/jobs/comment_update_job.rb
@@ -10,3 +10,4 @@ class CommentUpdateJob < ApplicationJob
   def render_comment(comment, current_user)
     CommentsController.render(partial: 'comments/comment', locals: { comment: comment, current_user: current_user })
   end
+end
