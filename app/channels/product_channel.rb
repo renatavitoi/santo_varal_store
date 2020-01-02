85536953a38612936bcb7b7ceaa9dd05bca98b63
renatavitoi <renata_band@hotmail.com>
2020-01-01 22:12:32 -0400

    6.14 fixed product_channel.rb

--- app/channels/product_channel.rb
+++ app/channels/product_channel.rb
@@ -4,12 +4,12 @@ class ProductChannel < ApplicationCable::Channel
     #stream_from "product_channel"
 
 
-  def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
-
+    def unsubscribed
+      # Any cleanup needed when channel is unsubscribed
+    end
 
     def listen (data)
       stop_all_streams
       stream_for data["product_id"]
+    end
   end
-end
