e47edf68ab5bfd755a0ed5f292acac86f6f6a8fb
renatavitoi <renata_band@hotmail.com>
2020-01-06 23:50:29 -0400

    6.14 fixed action cable

--- app/assets/javascripts/channels/product.js
+++ app/assets/javascripts/channels/product.js
@@ -1,13 +1,13 @@
 App.product = App.cable.subscriptions.create("ProductChannel", {
-  connected: function() {
+  connected: function () {
     // Called when the subscription is ready for use on the server
   },
-  
-  disconnected: function() {
+
+  disconnected: function () {
     // Called when the subscription has been terminated by the server
   },
-  
-  received: function(data) {
+
+  received: function (data) {
     // Called when there's incoming data on the websocket for this channel
     $(".alert.alert-info").show();
     //console.log(data);
@@ -15,10 +15,14 @@ App.product = App.cable.subscriptions.create("ProductChannel", {
     $("#average-rating").attr('data-score', data.average_rating);
     refreshRating();
   },
-  
-  listen_to_comments: function() {
+
+  listen_to_comments: function () {
     return this.perform('listen', {
       product_id: $("[data-product-id]").data("product-id")
     });
   }
+});
+
+$(document).on('turbolinks:load', function() {
+  App.product.listen_to_comments();
 });
